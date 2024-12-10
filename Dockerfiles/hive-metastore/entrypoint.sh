d9d9bd68369824b51dac9c4d1774bf32b6653c86
afif1731 <afifsdi@gmail.com>
2024-12-10 10:01:45 +0700

    fix: metastore ver

--- Dockerfiles/hive-metastore/entrypoint.sh
+++ Dockerfiles/hive-metastore/entrypoint.sh
@@ -7,17 +7,17 @@ export CLASSPATH=$HIVE_HOME/lib/mysql-connector-j-8.2.0.jar
 
 # Initialize schema if not already initialized
 echo "Checking Hive Metastore schema..."
-/opt/apache-hive-metastore-3.1.2-bin/bin/schematool -info -dbType mysql
+/opt/apache-hive-metastore-3.0.0-bin/bin/schematool -info -dbType mysql
 if [ $? -ne 0 ]; then
     echo "Initializing Hive Metastore schema..."
-    /opt/apache-hive-metastore-3.1.2-bin/bin/schematool -initSchema -dbType mysql
+    /opt/apache-hive-metastore-3.0.0-bin/bin/schematool -initSchema -dbType mysql
 else
     echo "Schema already initialized."
 fi
 
 # Start the Hive Metastore service
 echo "Starting Hive Metastore..."
-/opt/apache-hive-metastore-3.1.2-bin/bin/start-metastore
+/opt/apache-hive-metastore-3.0.0-bin/bin/start-metastore
 if [ $? -ne 0 ]; then
     echo "Failed to start Hive Metastore. Exiting."
     exit 1
